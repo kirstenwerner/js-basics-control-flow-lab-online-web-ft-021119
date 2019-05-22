6d1b934f121a9737dce29881525715f9bb5f32e8
kirstenwerner <kirstenmwerner@gmail.com>
2019-05-21 21:56:33 -0700

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  let ride
+  if (someValue <= 400) {
+    ride = 'This one is on me!';
+  }
+  else if (someValue > 2000 && someValue <= 2500) {
+    ride = 'I will gladly take your thirty bucks.'
+  }
+  else if (someValue > 2500) {
+    ride = 'No can do.'
+  }
+  return ride
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
   // Write your code here!
+  let response;
+  destination === 'NYC' ? response = 'Ok, sounds good.' : response = 'No go.';
+  return response
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let response;
+
+  switch (tip) {
+    case 'generous':
+      response = "Thank you so much.";
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+  }
+  return response
 }
